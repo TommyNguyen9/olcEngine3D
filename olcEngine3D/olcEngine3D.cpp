96a60de0287da86a9b60ed0e98e21c831df9b9c4
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-01 20:27:57 +0100

    Added mesh and functions

--- olcEngine3D/olcEngine3D.cpp
+++ olcEngine3D/olcEngine3D.cpp
@@ -1,8 +1,63 @@
-#include <iostream>
+#include "olcConsoleGameEngine.h"
+
+using namespace std;
+
+struct vec3d
+{
+    float x, y, z;
+};
+
+struct triangle
+{
+    vec3d p[3];
+};
+
+struct mesh
+{
+    vector<triangle> tris;
+};
+
+class olcEngine3D : public olcConsoleGameEngine
+{
+public:
+    olcEngine3D()
+    {
+        m_sAppName = L"3D Demo";
+    }
+
+private:
+    mesh meshCube;
+
+public:
+    bool OnUserCreate() override
+    {
+
+        meshCube.tris = {
+
+
+
+        };
+
+
+        return true;
+    }
+
+    bool OnUserUpdate(float fElapsedTime) override
+    {
+        return true;
+    }
+};
+
+
 
 int main()
 {
-    // test
-    std::cout << "Hello World!\n";
+    olcEngine3D demo;
+    if (demo.ConstructConsole(256, 240, 4, 4))
+        demo.Start();
+
+
+    return 0;
+    
 }
 
