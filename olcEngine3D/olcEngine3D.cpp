569c5e6225f7fbf30bc47d45b57052ff0b957cd7
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-02 19:29:43 +0100

    Added matrix vectors

--- olcEngine3D/olcEngine3D.cpp
+++ olcEngine3D/olcEngine3D.cpp
@@ -1,6 +1,5 @@
+#define OLC_PGE_APPLICATION
 #include "olcConsoleGameEngine.h"
-
-// test
 
 using namespace std;
 
@@ -19,6 +18,13 @@ struct mesh
     vector<triangle> tris;
 };
 
+
+struct mat4x4
+{
+    float m[4][4] = { 0 };
+};
+
+
 class olcEngine3D : public olcConsoleGameEngine
 {
 public:
@@ -29,6 +35,23 @@ public:
 
 private:
     mesh meshCube;
+    mat4x4 matProj;
+
+    void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m)
+    {
+        o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+        o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+        o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+        float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+        if (w != 0.0f)
+        {
+            o.x /= w; o.y /= w; o.z /= w;
+        }
+
+    }
+
+
 
 public:
     bool OnUserCreate() override
@@ -61,6 +84,20 @@ public:
             {1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
         };
 
+        // Projection Matrix
+        float fNear = 0.1f;
+        float fFar = 1000.0f;
+        float fFov = 90.0f;
+        float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+        float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+
+        matProj.m[0][0] = fAspectRatio * fFovRad;
+        matProj.m[1][1] = fFovRad;
+        matProj.m[2][2] = fFar / (fFar - fNear);
+        matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+        matProj.m[2][3] = 1.0f;
+        matProj.m[3][3] = 0.0f;
+
 
         return true;
     }
@@ -73,6 +110,16 @@ public:
         // Draw Triangles
         for (auto tri : meshCube.tris)
         {
+            triangle triProjected;
+            MultiplyMatrixVector(tri.p[0], triProjected.p[0], matProj);
+            MultiplyMatrixVector(tri.p[1], triProjected.p[1], matProj);
+            MultiplyMatrixVector(tri.p[2], triProjected.p[2], matProj);
+
+
+            DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+                triProjected.p[1].x, triProjected.p[1].y,
+                triProjected.p[2].x, triProjected.p[2].y,
+                PIXEL_SOLID, FG_WHITE);
 
         }
 
@@ -87,9 +134,9 @@ public:
 int main()
 {
     olcEngine3D demo;
-    if (demo.ConstructConsole(256, 240, 4, 4))
+    if (demo.ConstructConsole(135, 100, 8, 8))
         demo.Start();
-
+  
 
     return 0;
     
