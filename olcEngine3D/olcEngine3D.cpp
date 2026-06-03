cbd9d2d6f2a3e718ef62ab7177f434957fad295d
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-03 20:58:30 +0100

    Added quick inverse matrix

--- olcEngine3D/olcEngine3D.cpp
+++ olcEngine3D/olcEngine3D.cpp
@@ -185,6 +185,40 @@ private:
         return matrix;
     }
 
+    mat4x4 Matrix_PointAt(vec3d& pos, vec3d& target, vec3d& up)
+    {
+
+        // Calculate new forward direction
+        vec3d newForward = Vector_Sub(target, pos);
+        newForward = Vector_Normalise(newForward);
+
+        // Calculate new up direction
+        vec3d a = Vector_Mul(newForward, Vector_DotProduct(up, newForward));
+        vec3d newUp = Vector_Sub(up, a);
+        newUp = Vector_Normalise(newUp);
+
+        // New Right Direction - Cross product:
+        vec3d newRight = Vector_CrossProduct(newUp, newForward);
+
+        // Constructing Dimensioning & Translation Matrix:
+        mat4x4 matrix;
+        matrix.m[0][0] = newRight.x;   matrix.m[0][1] = newRight.y;    matrix.m[0][2] = newRight.z;    matrix.m[0][3] = 0.0f;
+        matrix.m[1][0] = newUp.x;      matrix.m[1][1] = newUp.y;       matrix.m[1][2] = newUp.z;       matrix.m[1][3] = 0.0f;
+        matrix.m[2][0] = newForward.x; matrix.m[2][1] = newForward.y;  matrix.m[2][2] = newForward.z;  matrix.m[2][3] = 0.0f;
+        matrix.m[3][0] = pos.x;        matrix.m[3][1] = pos.y;         matrix.m[3][2] = pos.z;         matrix.m[3][3] = 1.0f;
+        return matrix;
+    }
+
+    mat4x4 Matrix_QuickInverse(mat4x4& m) // Rotation/Translation Matrices only!
+    {
+        mat4x4 matrix;
+        matrix.m[0][0] = m.m[0][0]; matrix.m[0][1] = m.m[1][0]; matrix.m[0][2] = m.m[2][0]; matrix.m[0][3] = 0.0f;
+        matrix.m[1][0] = m.m[0][1]; matrix.m[1][1] = m.m[1][1]; matrix.m[1][2] = m.m[2][1]; matrix.m[1][3] = 0.0f;
+        matrix.m[2][0] = m.m[0][2]; matrix.m[2][1] = m.m[1][2]; matrix.m[2][2] = m.m[2][2]; matrix.m[2][3] = 0.0f;
+
+    }
+
+
 
     vec3d Vector_Add(vec3d &v1, vec3d &v2)
     {
@@ -288,7 +322,7 @@ public:
 
         // Set up rotation matrices:
         mat4x4 matRotZ, matRotX;
-        fTheta += 1.0f * fElapsedTime;
+        //fTheta += 1.0f * fElapsedTime;
 
         matRotZ = Matrix_MakeRotationZ(fTheta);
         matRotX = Matrix_MakeRotationX(fTheta);
