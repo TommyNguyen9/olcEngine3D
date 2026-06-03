46f02e59a0b21f7c02b1cdf72ab8ff0be53012cc
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-06-03 04:52:39 +0100

    Reading in object file into triangle mesh.

--- olcEngine3D/olcEngine3D.cpp
+++ olcEngine3D/olcEngine3D.cpp
@@ -1,6 +1,10 @@
 #define OLC_PGE_APPLICATION
 #include "olcConsoleGameEngine.h"
 
+#include <fstream>
+#include <strstream>
+
+
 using namespace std;
 
 struct vec3d
@@ -21,8 +25,40 @@ struct mesh
 {
     vector<triangle> tris;
 
-    bool LoadFromObjectFile(string sFileName)
+    bool LoadFromObjectFile(string sFilename)
     {
+        ifstream f(sFilename);
+        if (!f.is_open())
+            return false;
+
+        // Local cache of verts
+        vector<vec3d> verts;
+
+        while (!f.eof())
+        {
+            char line[128];
+            f.getline(line, 128);
+
+            strstream s;
+            s << line;
+
+            char junk;
+
+            if (line[0] == 'v')
+            {
+                vec3d v;
+                s >> junk >> v.x >> v.y >> v.z;
+                verts.push_back(v);
+            }
+
+            if (line[0] == 'f')
+            {
+                int f[3];
+                s >> junk >> f[0] >> f[1] >> f[2];
+                tris.push_back({ verts[f[0] - 1], verts[f[1] - 1], verts[f[2] - 1] });
+            }
+        }
+
         return true;
     }
 };
@@ -46,9 +82,9 @@ private:
     mesh meshCube;
     mat4x4 matProj;
 
-    vec3d vCamera;
+    vec3d vCamera = { 0.0f, 0.0f, 0.0f };
 
-    float fTheta;
+    float fTheta = 0.0f;
 
     void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m)
     {
